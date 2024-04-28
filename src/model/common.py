171d28a06ddd8b5e342aa7e708482e3182a2055a
ADulian <duli95@gmail.com>
2024-04-28 20:14:45 +0100

    Added upsample module

--- src/model/common.py
+++ src/model/common.py
@@ -1,7 +1,6 @@
 import torch.nn as nn
 
 
-
 # --------------------------------------------------------------------------------
 class Downsample(nn.Module):
     """ A simple convolutional block that downsamples feature maps by 2 (stride=2)
@@ -36,20 +35,20 @@ class Downsample(nn.Module):
 
         super().__init__()
 
-        # --- Attribs
-        self.norm = norm
-
         # --- Layers
         # Convolution Layer
         self.conv = nn.Conv2d(in_channels=in_channels,
                               out_channels=out_channels,
                               kernel_size=kernel_size,
-                              stride=2,
-                              padding= (kernel_size - 1) // 2,
+                              stride=2,  # Downsample by 2
+                              padding=(kernel_size - 1) // 2,  # Same padding
                               bias=False)  # ToDo: Check if this should be conditioned on norm
 
         # Normalisation layer (equivalent of GroupNorm where num_groups=out_channels
-        self.instance_norm = nn.InstanceNorm2d(num_features=out_channels)
+        if norm:
+            self.norm = nn.InstanceNorm2d(num_features=out_channels)
+        else:
+            self.norm = None
 
         # Activation layer
         self.act = nn.LeakyReLU()
@@ -73,7 +72,88 @@ class Downsample(nn.Module):
         out = self.conv(x)
 
         if self.norm:
-            out = self.instance_norm(out)
+            out = self.norm(out)
+
+        out = self.act(out)
+
+        return out
+
+
+# --------------------------------------------------------------------------------
+class Upsample(nn.Module):
+    """ A simple convolutional block that upsamples feature maps by 2 (kernel_size=4, stride=2)
+
+        For example
+            input_tensor [128, 128] -> output_tensor [256, 256]
+
+        Padding = same
+        Bias = False
+        Dropout = 0.5
+
+        Model
+            Conv2d -> InstanceNorm2d -> (opt) Dropout -> ReLU
+
+        """
+
+    # --------------------------------------------------------------------------------
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size=4,
+                 dropout=True):
+        """ Init upsample module
+
+        ---
+        Parameters
+            in_channels: Number of input channels
+            out_channels: Number of output channels
+            kernel_size: Kernel size
+            dropout: Whether to apply dropout
+        """
+
+        super().__init__()
+
+        # --- Layers
+        # Transpose Convolution Layer
+        self.conv = nn.ConvTranspose2d(in_channels=in_channels,
+                                       out_channels=out_channels,
+                                       kernel_size=kernel_size,
+                                       stride=2,  # Upsample by 2
+                                       padding=(kernel_size - 1) // 2,  # Same padding
+                                       bias=False)
+
+        # Normalisation layer (equivalent of GroupNorm where num_groups=out_channels
+        self.norm = nn.InstanceNorm2d(num_features=out_channels)
+
+        # Dropout
+        if dropout:
+            self.dropout = nn.Dropout(0.5)
+        else:
+            self.dropout = None
+
+        # Activation layer
+        self.act = nn.ReLU()
+
+    # --------------------------------------------------------------------------------
+    def forward(self,
+                x):
+        """ Upsample input tensor
+
+        ---
+        Parameters
+            x: Input tensor
+
+        ---
+        Returns
+            out: x upsampled by 2
+
+        """
+
+        out = self.conv(x)
+        out = self.norm(out)
+
+        if self.dropout:
+            out = self.dropout(out)
 
         out = self.act(out)
 
--- src/tests/common.py
+++ src/tests/common.py
@@ -4,7 +4,7 @@
 import torch
 import torch.nn as nn
 
-from src.model.common import Downsample
+from src.model.common import Downsample, Upsample
 
 class TestDownsample:
     """ Unit tests for the Downsample class
@@ -18,7 +18,8 @@ class TestDownsample:
 
         downsample = Downsample(in_channels=3, out_channels=64, kernel_size=3, norm=True)
         assert isinstance(downsample, nn.Module)
-        assert downsample.norm == True
+        assert downsample.conv.kernel_size[0] == 3
+        assert downsample.norm.num_features == 64
 
     def test_forward(self):
         """Test that the forward method downsamples the input tensor correctly
@@ -60,8 +61,11 @@ class TestDownsample:
         """
 
         downsample = Downsample(in_channels=3, out_channels=64, kernel_size=3, norm=True)
-        assert isinstance(downsample.instance_norm, nn.InstanceNorm2d)
-        assert downsample.instance_norm.num_features == 64
+        assert isinstance(downsample.norm, nn.InstanceNorm2d)
+        assert downsample.norm.num_features == 64
+
+        downsample = Downsample(in_channels=3, out_channels=64, kernel_size=3, norm=False)
+        assert downsample.norm is None
 
     def test_activation_layer(self):
         """Test that the activation layer is configured correctly
@@ -69,5 +73,40 @@ class TestDownsample:
         Verifies that the activation layer is a LeakyReLU layer
         """
 
-        downsample = Downsample(in_channels=3, out_channels=64, kernel_size=3, norm=True)
+        downsample = Downsample(in_channels=3, out_channels=64, kernel_size=3)
         assert isinstance(downsample.act, nn.LeakyReLU)
+
+class TestUpsample:
+    def test_init(self):
+        upsample = Upsample(in_channels=3, out_channels=64, kernel_size=3, dropout=True)
+        assert isinstance(upsample, nn.Module)
+        assert upsample.conv.kernel_size[0] == 3
+        assert upsample.norm.num_features == 64
+        assert upsample.dropout.p == 0.5 if upsample.dropout else None
+
+    def test_forward(self):
+        upsample = Upsample(in_channels=3, out_channels=64)
+        input_tensor = torch.randn(1, 3, 128, 128)
+        output = upsample(input_tensor)
+        assert output.shape == (1, 64, 256, 256)
+
+    def test_forward_no_dropout(self):
+        upsample = Upsample(in_channels=3, out_channels=64, dropout=False)
+        input_tensor = torch.randn(1, 3, 128, 128)
+        output = upsample(input_tensor)
+        assert output.shape == (1, 64, 256, 256)
+
+    def test_conv_layer(self):
+        upsample = Upsample(in_channels=3, out_channels=64, kernel_size=3)
+        assert isinstance(upsample.conv, nn.ConvTranspose2d)
+        assert upsample.conv.stride == (2, 2)
+        assert upsample.conv.padding == (1, 1)
+
+    def test_instance_norm_layer(self):
+        upsample = Upsample(in_channels=3, out_channels=64, kernel_size=3)
+        assert isinstance(upsample.norm, nn.InstanceNorm2d)
+        assert upsample.norm.num_features == 64
+
+    def test_activation_layer(self):
+        upsample = Upsample(in_channels=3, out_channels=64, kernel_size=3)
+        assert isinstance(upsample.act, nn.ReLU)
