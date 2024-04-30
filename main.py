b0e0aab9af22c68bcd291f8105b88e7339ae5ebc
ADulian <duli95@gmail.com>
2024-04-30 21:00:17 +0100

    Small improvements to the argparser

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 Main entrypoint, for now just training
 """
 import argparse
+import sys
+
 import torch
 
 from src.tasks.trainer import Trainer
@@ -10,17 +12,19 @@ from src.tasks.trainer import Trainer
 if __name__ == '__main__':
 
     # --- Args
-    parser = argparse.ArgumentParser(description='Simple implementation of CycleGAN in PyTorch')
+    parser = argparse.ArgumentParser(description='Simple implementation of CycleGAN in PyTorch',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
     parser.add_argument('--data_root', type=str, default="data/",
                         help='Root path to dataset')
     parser.add_argument('--num_epochs', type=int, default=1,
                         help='Number of training epochs')
     parser.add_argument('--batch_size', type=int, default=64,  # Max that fits onto 24GB GPU
-                        help='Batch size for processing data (default: 64)')
+                        help='Batch size for processing data, mine\'s set to max for 3090 24GB')
     parser.add_argument('--weights_path', type=str, default=None, # Path to load weights
-                        help='Load weights path')
+                        help='Weights path, loads only if not None')
     parser.add_argument('--output_path', type=str, default=None,  # Path to saving data e.g. weights
-                        help='Save metadata in here')
+                        help='Save all data in "." if None')
 
 
     args = parser.parse_args()
