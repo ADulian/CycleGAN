081bdfc17fc2bbd61f8541972bf5ae41d528704b
ADulian <duli95@gmail.com>
2024-05-01 12:07:56 +0100

    Added useful functions to dataset

--- src/dataset/monet_dataset.py
+++ src/dataset/monet_dataset.py
@@ -5,9 +5,11 @@ A torch Dataset class for loading monet and photos data
 import cv2
 import random
 import torch
+import numpy as np
 
 from pathlib import Path
 from torch.utils.data import Dataset
+
 
 # --------------------------------------------------------------------------------
 class MonetDataset(Dataset):
@@ -61,6 +63,90 @@ class MonetDataset(Dataset):
         x_monet = self.to_tensor(self.read_img(path_monet))
 
         return x_monet, x_photo
+
+    # --------------------------------------------------------------------------------
+    def get_sample(self,
+                   idx=0,
+                   as_tensor=True,
+                   sample_type="monet"):
+        """ Retrieves either Monet or Photo sample
+
+        ---
+        Parameters
+            idx: Sample idx
+            sample_type: Can be either monet or photo
+            as_tensor: True will return a 3D torch.Tensor [C, H, W] (-1,1)
+                else a cv2 RGB image (numpy array) [H, W, C] (0, 255)
+
+        ---
+        Returns
+            torch.Tensor | numpy.ndarray: A single sample as torch or numpy
+
+        """
+
+        # Get path to sample
+        if sample_type == "monet":
+            sample_path = self.monet_paths[idx]
+        elif sample_type == "photo":
+            sample_path = self.photo_paths[idx]
+        else:
+            raise ValueError(f"Sample type can be either monet or photo, given: {sample_type}")
+
+        # Read
+        sample = self.read_img(path=sample_path)
+
+        # Return as a 3D torch.Tensor [C, H, W]
+        if as_tensor:
+            return self.to_tensor(x=sample)
+
+        # Return as a 3D numpy array
+        return sample
+
+    # --------------------------------------------------------------------------------
+    def get_samples(self,
+                    as_tensor=True,
+                    batch_size=4,
+                    sample_type="monet"):
+        """ Retrieves all or N (batch_size) random samples of either monet or photo
+
+        ---
+        Parameters
+            as_tensor: True will return 4D torch.Tensor [N, C, H, W] (-1,1)
+                else numpy.ndarray [N, H, W, C] (0,255)
+            batch_size: If not None will return a random batch of n requested samples
+            sample_type: Can be either monet or photo
+
+        ---
+        Returns
+            torch.Tensor | numpy.ndarray: All samples as a single Tensor
+        """
+
+        # Get paths of type
+        if sample_type == "monet":
+            sample_paths = self.monet_paths
+        elif sample_type == "photo":
+            sample_paths = self.photo_paths
+        else:
+            raise ValueError(f"Sample type can be either monet or photo, given: {sample_type}")
+
+        # Check if batch was given
+        if batch_size is not None:
+            # Try to randomly sample
+            try:
+                sample_paths = random.sample(sample_paths, batch_size)
+            # Negative or too large
+            except ValueError as e:
+                raise ValueError(f"{e}. Number of samples: {len(sample_paths)}")
+
+        # Read sampels
+        monet_samples = [self.read_img(path=path) for path in sample_paths]
+
+        # Return as a 4D torch.Tensor
+        if as_tensor:
+            return torch.stack([self.to_tensor(t) for t in monet_samples], dim=0)
+
+        # Return as a 4D numpy.ndarray
+        return np.array(monet_samples)
 
     # --------------------------------------------------------------------------------
     def to_tensor(self, x):
